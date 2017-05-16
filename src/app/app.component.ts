3c4a3a2c6b5afc2252cd33ba222292fe20ac0a11
cyberplanner <sal@salweb.co.uk>
2017-05-16 13:11:43 +0100

    Clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,15 @@ export class Hero {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   template: `
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>ID: </label>{{hero.id}}</div>
-  <div><label>Name: </label>{{hero.name}}</div>
-  <input [(ngModel)] = "hero.name" placeholder="Name"/>
-  `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>ID: </label>{{hero.id}}</div>
+    <div><label>Name: </label>{{hero.name}}</div>
+    <div>
+      <label>Enter name:</label>
+      <input [(ngModel)] = "hero.name" placeholder="Name"/>
+    </div>
+    `
 })
 
 export class AppComponent {
