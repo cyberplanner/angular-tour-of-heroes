03cbb34cdc594f80abb047d57862076d4a129f98
cyberplanner <sal@salweb.co.uk>
2017-05-16 12:45:18 +0100

    create Hero class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'WildStorm'
+  };
 }
