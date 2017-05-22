e82b7ab863586bbfdb25dc53cc077d46a0c10a79
cyberplanner <sal@salweb.co.uk>
2017-05-22 13:43:01 +0100

    Act on the Promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -96,6 +96,6 @@ export class AppComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 }
